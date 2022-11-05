85af7bedd44f5e040c12e715dc87b75a4f7cecbd
Iuliaan Roman <iuliana.roman5@gmail.com>
2022-11-05 22:43:16 +0200

    -added new route for add animals, still needed some work to do.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,42 @@ import './App.css';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
 import Content from './components/Content';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddAnimal from './components/AddAnimal';
+
+
 
 function App() {
 
-const searchFilter = (filter) => {
+  // const [show, setShow] = useState(false);
 
-  console.log("Filtered by -> " , filter);
-}
+  // const handleCloseModal = () => setShow(false);
+
+  const searchFilter = (filter) => {
+
+    console.log("Filtered by -> ", filter);
+  }
 
   return <>
-  <Header />
-  <NavBar searchFilter={searchFilter}/>
-  <Content />
+    <Header />
+    {/* <AddModal
+            display={show}
+            hide={handleCloseModal}
+
+        /> */}
+    <BrowserRouter>
+      <Routes >
+        <Route path='/' element={<>
+          <NavBar searchFilter={searchFilter} />
+          <Content  />
+        </>} />
+        <Route path='/add-animal' element={<AddAnimal />} />
+      </Routes>
+    </BrowserRouter>
+
+
+
   </>
 }
 
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,20 +1,42 @@
 import Card from './Card';
 import plusImg from "../assets/+.svg";
+import Button from "../UI/Button";
+import { useNavigate } from "react-router-dom";
 
-const Content = () => {
+
+const Content = (props) => {
+    const navigate = useNavigate();
+
     const array = [{ id: '1', name: 'Oxi', gender: 'M', breed: 'Labrador', age: '2 months' }];
+    
 
-    return (
+    const goToAddAnimal = () => {
+        navigate('add-animal')
+    }
+
+
+    return (<>
+       
+
         <div className="content-box">
-            <div className='card1 first'><img src={plusImg} /> </div>
+            <div className='card1 first'>
+                <img src={plusImg} />
+                <Button
+                    class={"add-button"}
+                    btnName={"Add animal"}
+                    onClick={goToAddAnimal}
+                />
+            </div>
             {array.map((obj, index) => <Card
-            breed={obj.breed}
-            age={obj.age}
-            name={obj.name}
-            gender={obj.gender}
-            key={index} 
+                breed={obj.breed}
+                age={obj.age}
+                name={obj.name}
+                gender={obj.gender}
+                key={index}
             />)}
         </div>
+      
+    </>
     );
 }
 
